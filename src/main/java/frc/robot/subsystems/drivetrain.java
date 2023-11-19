c23d455098e311e655412200e63af67afbbd94ff
Tanish Mittal <mittal.tanish@gmail.com>
2023-11-18 22:36:13 -0500

    initial

---
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -0,0 +1,23 @@
+
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class drivetrain extends SubsystemBase {
+
+    public drivetrain() {
+        
+    }
+
+    @Override
+    public void periodic() {
+      
+    }
+
+    @Override
+    public void simulationPeriodic() {
+       
+    }
+}
