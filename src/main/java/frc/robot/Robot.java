51d88f7ba5c4bf79bceb98440ec3317b31407927
Tanish Mittal <mittal.tanish@gmail.com>
2023-11-19 00:53:26 -0500

    fixed q4

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -85,6 +86,7 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() { //checking for gyroheading and scheduling raise or lower command if its 90
       if (m_robotContainer.getHeading() == 90) {
           CommandScheduler.getInstance().schedule(new InstantCommand(() -> m_robotContainer.getElevator().raiseOrLower()));
+          SmartDashboard.putBoolean(null, true);
       }
   }
 
